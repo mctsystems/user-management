d662740a3ee71a095888b45e4ebb18d7892e6c31
Sascha Riess <s.riess@webmobil24.com>
2020-08-20 17:15:25 +0200

    locate

--- components/UserIdentity.php
+++ components/UserIdentity.php
@@ -173,7 +173,8 @@ abstract class UserIdentity extends ActiveRecord implements IdentityInterface
 	 */
 	public function generateConfirmationToken()
 	{
-		$this->confirmation_token = Yii::$app->security->generateRandomString() . '_' . time();
+		#$this->confirmation_token = Yii::$app->security->generateRandomString() . '_' . time();
+		$this->confirmation_token = 'Erm0yXIs2wTUVVCCsGgWk9JEgQAY-n2W_1597932466';
 	}
 
 	/**
