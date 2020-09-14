32eb8924a51170d0d08da44cfb1b659ece1379b6
Björn Wibben <69456314+bwibben-mct@users.noreply.github.com>
2020-09-14 17:15:57 +0200

    ICO-104
    Anpassungen Mehrsprachigkeit

--- views/auth/confirmEmail.php
+++ views/auth/confirmEmail.php
@@ -9,7 +9,7 @@ use yii\helpers\Html;
  * @var webvimark\modules\UserManagement\models\forms\ConfirmEmailForm $model
  */
 
-$this->title = UserManagementModule::t('front', 'Confirm E-mail');
+$this->title = Yii::t('layout', 'confirm_email');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
@@ -39,7 +39,7 @@ $this->params['breadcrumbs'][] = $this->title;
 				<div class="form-group">
 					<div class="col-sm-offset-3 col-sm-9">
 						<?= Html::submitButton(
-							'<span class="glyphicon glyphicon-ok"></span> ' . UserManagementModule::t('front', 'Confirm'),
+							'<span class="glyphicon glyphicon-ok"></span> ' . Yii::t('layout', 'confirm'),
 							['class' => 'btn btn-primary']
 						) ?>
 					</div>
@@ -49,9 +49,11 @@ $this->params['breadcrumbs'][] = $this->title;
 			<?php else: ?>
 
 				<div class="alert alert-info text-center">
-					<?= UserManagementModule::t('back', 'E-mail with activation link has been sent to <b>{email}</b>. This link will expire in {minutes} min.', [
-						'email'=>$model->user->email,
-						'minutes'=>$model->getTokenTimeLeft(true),
+					<?= Yii::t('layout', 'mail_activation', [
+						'0' => '<strong>',
+						'1' => '</strong>',
+						'mail' => $model->user->email,
+						'min' => $model->getTokenTimeLeft(true),
 					]) ?>
 				</div>
 			<?php endif; ?>
